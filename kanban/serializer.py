8e97104d7532969ee95e41febae32325b2114555
William Holland <william.holland@codethink.co.uk>
2014-10-20 12:02:51 +0100

    Added Serializer
    The serializer classes will allow data to be parsed and parsed data to
    be put into the database.

---
+++ kanban/serializer.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class KanbanSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Kanban
+
+class LaneSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Lane
+
+class MilestoneSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Milestone
+
+class BucketSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Bucket
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Card
+
+class ChecklistSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Checklist
+
+class TickEventSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = TickEvent
+
+class AssigneesSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Assignees
+
+class PermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Permissions
