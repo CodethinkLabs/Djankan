743dba884f659ed06bb1b38834e342aad3b647db
William Holland <william.holland@codethink.co.uk>
2014-10-21 16:27:01 +0100

    Changed Permissions
    created an all-encompassing permission class to fit our model

--- kanban/permissions.py
+++ kanban/permissions.py
@@ -1,19 +1,36 @@
 from rest_framework import permissions
+import re
 
-class editPermission(permissions.BasePermission):
+class defaultPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        result = Permissions.objects.filter(
-            kanban=request.kanban,
-            user=request.user,
+        user = request.user
+        path = request.path
+        # if in the user/ subpath any user can GET but for any other
+        # method the request user must be an admin or the user who's
+        # data is being edited
+        if re.match(r'^user/', path):
+            if request.method = 'GET':
+                return True
+            userPath = re.search(r'^users/(.+?)/',path)
+            if user == userPath:
+                return True
+            return False
+        # if in the kanban/ subpath anybody with any role on the kanban
+        # can GET but for any other method the request user must be an
+        # Editor
+        try:
+            findKanbanID = re.search(r'^kanban/(.+?)/', path)
+            kanbanID = findKanbanID.group(1)
+        except:
+            return False
+        if request.method = 'GET':
+            return Permissions.objects.filter(
+                user=user,
+                kanban=kanbanID
+                ).exists()
+   
+        return Permissions.objects.filter(
+            user=user,
+            kanban=kanbanID,
             USER_ROLE='EDIT'
             ).exists()
-        return result
-
-class viewPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        result = Permissions.objects.filter(
-            kanban=request.kanban,
-            user=request.user,
-            USER_ROLE='VIEW'
-            ).exists()
-        return result 
