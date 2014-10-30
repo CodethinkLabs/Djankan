a604bd093976c8af7762dcd25388f5f0d2747294
William Holland <william.holland@codethink.co.uk>
2014-10-30 11:26:31 +0000

    Board Methods
    the API methods relating to boards have been hooked up to their URLs

--- djankan/urls.py
+++ djankan/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 def idof(objectName):
-    return r'(?P<'+objectName+r'_id>.+?)/'
+    return r'(?P<'+objectName+r'_id>[0-9]+)/'
 
 v1Prefix = r'^api/v1/'
 
@@ -28,6 +28,18 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(boards+'$',views.boardAPIView),
+    url(boards+idof('board')+'$',views.boardIDView),
+    url(boards+idof('board')+'roles/$',views.boardRolesView),
+    url(boards+idof('board')+'roles/'+idof('role')+'$',views.boardRolesIDView),
+    url(milestones+'$',views.boardMilestonesView),
+    url(milestones+idof('milestone')+'$',views.boardMilestoneIDView),
+    url(milestones+idof('milestone')+'cards/$',views.boardMilestoneCardView),
+    url(buckets+'$',views.boardBucketsView),
+    url(buckets+idof('bucket')+'$',views.boardBucketIDView),
+    url(buckets+idof('bucket')+'cards/$',views.boardBucketCardView),
+    url(lanes+'$',views.boardLanesView),
+    url(lanes+idof('lane')+'$',boardLaneIDView),
+    url(lanes+idof('lane')+'cards/$',boardLaneCardView),
     url(cards+'$',views.boardCardAPIView),
     url(card+'$',views.cardAPIView),
 )
--- kanban/views/boards.py
+++ kanban/views/boards.py
@@ -4,18 +4,92 @@ from rest_framework.response import Response
 from kanban.models import *
 from kanban.serializers import *
 from django.http import Http404
+from methods import *
+
+def get_by_board(request,Class,board_id):
+    return get_filter_method(request,Class,'board',board_id)
 
 @api_view(['POST', 'GET'])
 def boardAPIView(request):
     if request.method == 'POST':
-        print "POST"
-        serializer = BoardSerializer(data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return post_method(request,Board)
     elif request.method == 'GET':
-        print "GET"
-        boards = Board.objects.all()
-        serializer = BoardSerializer(boards, many=True)
-        return Response(serializer.data)
+        return get_all_method(request,Board)
+
+@api_view(['GET','PUT'])
+def boardIDView(request,board_id):
+    if request.method == 'GET':
+        return get_one_method(request,Board,board_id)
+    elif request.method == 'PUT':
+        return post_method(request,Board)
+
+@api_view(['GET','POST'])
+def boardRolesView(request,board_id):
+    if request.method == 'GET':
+        return get_by_board(request,Permissions,board_id)
+    elif request.method == 'POST':
+        return post_method(request,Permissions)
+
+@api_view(['GET','PUT'])
+def boardRolesIDView(request,board_id,role_id):
+    if request.method == 'GET':
+        return get_by_board(request,Permissions,board_id)
+    elif request.method == 'PUT':
+        return put_method(request,Permissions,role_id)
+
+@api_view(['GET','POST'])
+def boardMilestonesView(request,board_id):
+    if request.method == 'GET':
+        return get_by_board(request,Milestone,board_id)
+    elif request.method == 'POST':
+        return post_method(request,Milestone)
+
+@api_view(['GET','PUT'])
+def boardMilestoneIDView(request,board_id,milestone_id):
+    if request.method == 'GET':
+        return get_one_method(request,Milestone,milestone_id)
+    elif request.method == 'PUT':
+        return put_method(request,Milestone,milestone_id)
+
+@api_view(['GET'])
+def boardMilestoneCardView(request,board_id,milestone_id):
+    if request.method == 'GET':
+        return get_filter_method(request,Card,'milestone',milestone_id)
+
+@api_view(['GET','POST'])
+def boardBucketsView(request,board_id):
+    if request.method == 'GET':
+        return get_by_board(request,Bucket,board_id)
+    elif request.method == 'POST':
+        return post_method(request,Bucket)
+
+@api_view(['GET','PUT'])
+def boardBucketIDView(request,board_id,bucket_id):
+    if request.method == 'GET':
+        return get_one_method(request,Bucket,bucket_id)
+    if request.method == 'PUT':
+        return put_method(request,Bucket,bucket_id)
+
+@api_view(['GET'])
+def boardBucketCardView(request,board_id,bucket_id):
+    if request.method == 'GET':
+        return get_filter_method(request,Card,'bucket',bucket_id)
+
+@api_view('GET','POST'])
+def boardLanesView(request,board_id):
+    if request.method == 'GET':
+        return get_by_board(request,Lane,board_id)
+    elif request.method == 'POST':
+        return post_method(request,Lane)
+
+@api_view('GET','PUT')
+def boardLaneIDView(request,board_id,lane_id):
+    if request.method == 'GET':
+        return get_one_method(request,Lane,lane_id)
+    elif request.method == 'PUT':
+        return put_method(request,Lane,lane_id)
+
+@api_view('GET')
+def boardLaneCardView(request,board_id,lane_id):
+    if request.method == 'GET':
+        return get_filter_method(request,Card,'lane',lane_id)
