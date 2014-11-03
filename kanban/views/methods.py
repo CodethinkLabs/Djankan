5520c15b426d9fa105e455b7bfb519c685f41172
Craig Griffiths <craig.griffiths@codethink.co.uk>
2014-11-03 16:39:23 +0000

    removed general exceptions and gave some variables more descriptive names in methods.py, also ran methods.py through pep8 and made whitespace tweaks

--- kanban/views/methods.py
+++ kanban/views/methods.py
@@ -3,25 +3,27 @@ from rest_framework.response import Response
 from kanban.models import *
 from kanban.serializers import *
 from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
-# find which serializer to use
+
 def serializer_of(Class):
-    serializer_tuples = [ 
-        (Board,BoardSerializer),
-        (Lane,LaneSerializer),
-        (Milestone,MilestoneSerializer),
-        (Bucket,BucketSerializer),
-        (Card,CardSerializer),
-        (Checklist,ChecklistSerializer),
-        (TickEvent,TickEventSerializer),
-        (Assignees,AssigneesSerializer),
-        (Permissions,PermissionsSerializer),
+    serializer_tuples = [
+        (Board, BoardSerializer),
+        (Lane, LaneSerializer),
+        (Milestone, MilestoneSerializer),
+        (Bucket, BucketSerializer),
+        (Card, CardSerializer),
+        (Checklist, ChecklistSerializer),
+        (TickEvent, TickEventSerializer),
+        (Assignees, AssigneesSerializer),
+        (Permissions, PermissionsSerializer),
     ]
-    
-    a = [item[1] for item in serializer_tuples if Class in item]
-    return a[0]
 
-def post_method(request,Class): 
+    serializer = [item[1] for item in serializer_tuples if Class in item]
+    return serializer[0]
+
+
+def post_method(request, Class):
     serializer_name = serializer_of(Class)
     serializer = serializer_name(data=request.DATA)
     if serializer.is_valid():
@@ -29,45 +31,49 @@ def post_method(request,Class):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def get_all_method(request,Class):
-    serializer_name = serializer_of(Class)
-    try:
-        q = Class.objects.all()
-    except:
-        content = {'error': 'No matching data'}
-        return Response(content,status=status.HTTP_404_NOT_FOUND)
-    serializer = serializer_name(q,many=True)
-    return Response(serializer.data)
 
-def get_one_method(request,Class,id):
+def get_all_method(request, Class):
     serializer_name = serializer_of(Class)
     try:
-        q = Class.objects.get(id=id)
-    except:
+        instances = Class.objects.all()
+    except ObjectDoesNotExist:
         content = {'error': 'No matching data'}
         return Response(content, status=status.HTTP_404_NOT_FOUND)
-    serializer = serializer_name(q)
+    serializer = serializer_name(instances, many=True)
     return Response(serializer.data)
 
-def get_filter_method(request,Class,field,filterby):
+
+def get_one_method(request, Class, id):
+    serializer_name = serializer_of(Class)
+    try:
+        instance = Class.objects.get(id=id)
+    except ObjectDoesNotExist:
+        content = {'error': 'No matching data'}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+    serializer = serializer_name(instances)
+    return Response(serializer.data)
+
+
+def get_filter_method(request, Class, field, filterby):
     serializer_name = serializer_of(Class)
     filter_dict = {field: filterby}
     try:
-        q = Class.objects.filter(**filter_dict)
-    except:
+        instances = Class.objects.filter(**filter_dict)
+    except ObjectDoesNotExist:
         content = {'error': 'No matching data'}
         return Response(content, status=status.HTTP_404_NOT_FOUND)
-    serializer = serializer_name(q,many=True)
+    serializer = serializer_name(instances, many=True)
     return Response(serializer.data)
 
-def put_method(request,Class,id):
+
+def put_method(request, Class, id):
     serializer_name = serializer_of(Class)
     try:
-        q = Class.objects.get(id=id)
-    except:
+        instance = Class.objects.get(id=id)
+    except ObjectDoesNotExist:
         content = {'error': 'No matching data'}
         return Response(content, status=status.HTTP_404_NOT_FOUND)
-    serializer = serializer_name(q,data=request.DATA)
+    serializer = serializer_name(instance, data=request.DATA)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
