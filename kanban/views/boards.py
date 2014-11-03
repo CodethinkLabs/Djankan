de07bfcf2422197eafd51aeb0ea07bd843be0fc8
Craig Griffiths <craig.griffiths@codethink.co.uk>
2014-11-03 18:14:42 +0000

    run boards.py through pep8 for style consistency

--- kanban/views/boards.py
+++ kanban/views/boards.py
@@ -6,90 +6,104 @@ from kanban.serializers import *
 from django.http import Http404
 from methods import *
 
-def get_by_board(request,Class,board_id):
-    return get_filter_method(request,Class,'board',board_id)
+
+def get_by_board(request, Class, board_id):
+    return get_filter_method(request, Class, 'board', board_id)
+
 
 @api_view(['POST', 'GET'])
 def boardAPIView(request):
     if request.method == 'POST':
-        return post_method(request,Board)
+        return post_method(request, Board)
     elif request.method == 'GET':
-        return get_all_method(request,Board)
+        return get_all_method(request, Board)
 
-@api_view(['GET','PUT'])
-def boardIDView(request,board_id):
+
+@api_view(['GET', 'PUT'])
+def boardIDView(request, board_id):
     if request.method == 'GET':
-        return get_one_method(request,Board,board_id)
+        return get_one_method(request, Board, board_id)
     elif request.method == 'PUT':
-        return post_method(request,Board)
+        return post_method(request, Board)
 
-@api_view(['GET','POST'])
-def boardRolesView(request,board_id):
+
+@api_view(['GET', 'POST'])
+def boardRolesView(request, board_id):
     if request.method == 'GET':
-        return get_by_board(request,Permissions,board_id)
+        return get_by_board(request, Permissions, board_id)
     elif request.method == 'POST':
-        return post_method(request,Permissions)
+        return post_method(request, Permissions)
 
-@api_view(['GET','PUT'])
-def boardRolesIDView(request,board_id,role_id):
+
+@api_view(['GET', 'PUT'])
+def boardRolesIDView(request, board_id, role_id):
     if request.method == 'GET':
-        return get_by_board(request,Permissions,board_id)
+        return get_by_board(request, Permissions, board_id)
     elif request.method == 'PUT':
-        return put_method(request,Permissions,role_id)
+        return put_method(request, Permissions, role_id)
 
-@api_view(['GET','POST'])
-def boardMilestonesView(request,board_id):
+
+@api_view(['GET', 'POST'])
+def boardMilestonesView(request, board_id):
     if request.method == 'GET':
-        return get_by_board(request,Milestone,board_id)
+        return get_by_board(request, Milestone, board_id)
     elif request.method == 'POST':
-        return post_method(request,Milestone)
+        return post_method(request, Milestone)
 
-@api_view(['GET','PUT'])
-def boardMilestoneIDView(request,board_id,milestone_id):
+
+@api_view(['GET', 'PUT'])
+def boardMilestoneIDView(request, board_id, milestone_id):
     if request.method == 'GET':
-        return get_one_method(request,Milestone,milestone_id)
+        return get_one_method(request, Milestone, milestone_id)
     elif request.method == 'PUT':
-        return put_method(request,Milestone,milestone_id)
+        return put_method(request, Milestone, milestone_id)
+
 
 @api_view(['GET'])
-def boardMilestoneCardView(request,board_id,milestone_id):
+def boardMilestoneCardView(request, board_id, milestone_id):
     if request.method == 'GET':
-        return get_filter_method(request,Card,'milestone',milestone_id)
+        return get_filter_method(request, Card, 'milestone', milestone_id)
 
-@api_view(['GET','POST'])
-def boardBucketsView(request,board_id):
+
+@api_view(['GET', 'POST'])
+def boardBucketsView(request, board_id):
     if request.method == 'GET':
-        return get_by_board(request,Bucket,board_id)
+        return get_by_board(request, Bucket, board_id)
     elif request.method == 'POST':
-        return post_method(request,Bucket)
+        return post_method(request, Bucket)
 
-@api_view(['GET','PUT'])
-def boardBucketIDView(request,board_id,bucket_id):
+
+@api_view(['GET', 'PUT'])
+def boardBucketIDView(request, board_id, bucket_id):
     if request.method == 'GET':
-        return get_one_method(request,Bucket,bucket_id)
+        return get_one_method(request, Bucket, bucket_id)
     if request.method == 'PUT':
-        return put_method(request,Bucket,bucket_id)
+        return put_method(request, Bucket, bucket_id)
+
 
 @api_view(['GET'])
-def boardBucketCardView(request,board_id,bucket_id):
+def boardBucketCardView(request, board_id, bucket_id):
     if request.method == 'GET':
-        return get_filter_method(request,Card,'bucket',bucket_id)
+        return get_filter_method(request, Card, 'bucket', bucket_id)
 
-@api_view(['GET','POST'])
-def boardLanesView(request,board_id):
+
+@api_view(['GET', 'POST'])
+def boardLanesView(request, board_id):
     if request.method == 'GET':
-        return get_by_board(request,Lane,board_id)
+        return get_by_board(request, Lane, board_id)
     elif request.method == 'POST':
-        return post_method(request,Lane)
+        return post_method(request, Lane)
 
-@api_view(['GET','PUT'])
-def boardLaneIDView(request,board_id,lane_id):
+
+@api_view(['GET', 'PUT'])
+def boardLaneIDView(request, board_id, lane_id):
     if request.method == 'GET':
-        return get_one_method(request,Lane,lane_id)
+        return get_one_method(request, Lane, lane_id)
     elif request.method == 'PUT':
-        return put_method(request,Lane,lane_id)
+        return put_method(request, Lane, lane_id)
+
 
 @api_view(['GET'])
-def boardLaneCardView(request,board_id,lane_id):
+def boardLaneCardView(request, board_id, lane_id):
     if request.method == 'GET':
-        return get_filter_method(request,Card,'lane',lane_id)
+        return get_filter_method(request, Card, 'lane', lane_id)
